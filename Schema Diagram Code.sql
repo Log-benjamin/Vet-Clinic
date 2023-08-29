dbb200bc51842472d7deaacae17fe7564340f728
Uwimbabazi <uwimbabazidorcas65@gmail.com>
2023-08-29 10:54:03 +0200

    Add schema diagrams

--- Schema Diagram Code.sql
+++ Schema Diagram Code.sql
@@ -21,6 +21,32 @@ CREATE TABLE "species" (
   "name" VARCHAR(50)
 );
 
+CREATE TABLE "vets" (
+  "id" integer PRIMARY KEY,
+  "name" VARCHAR(50),
+  "age" integer,
+  "date_of_graduation" date
+);
+
+CREATE TABLE "specializations" (
+  "species_id" integer,
+  "vet_id" integer
+);
+
+CREATE TABLE "visits" (
+  "vet_id" integer,
+  "animal_id" integer,
+  "visit_date" date
+);
+
 ALTER TABLE "animals" ADD FOREIGN KEY ("species_id") REFERENCES "species" ("id");
 
 ALTER TABLE "animals" ADD FOREIGN KEY ("owner_id") REFERENCES "owners" ("id");
+
+ALTER TABLE "specializations" ADD FOREIGN KEY ("species_id") REFERENCES "species" ("id");
+
+ALTER TABLE "specializations" ADD FOREIGN KEY ("vet_id") REFERENCES "vets" ("id");
+
+ALTER TABLE "visits" ADD FOREIGN KEY ("vet_id") REFERENCES "vets" ("id");
+
+ALTER TABLE "visits" ADD FOREIGN KEY ("animal_id") REFERENCES "animals" ("id");
